d29f279d0041effa81a61f1d56ac9bfc08607661
LukeTheWalker <lucag2001@gmail.com>
2023-05-09 12:35:00 +0200

    fix: memory leak

--- include/merge_contours.cu
+++ include/merge_contours.cu
@@ -330,6 +330,8 @@ void merge_contours_wrapper(point * d_contours, int * h_contours_size, int merge
     err = cudaFree(d_contours_out); cuda_err_check(err, __FILE__, __LINE__);
     err = cudaFree(d_cumulative_poisitions); cuda_err_check(err, __FILE__, __LINE__);
     err = cudaFree(d_merge_to); cuda_err_check(err, __FILE__, __LINE__);
+    err = cudaFree(d_closeness_matrix); cuda_err_check(err, __FILE__, __LINE__);
+    err = cudaFree(d_contours_sizes); cuda_err_check(err, __FILE__, __LINE__);
 
     #if PROFILING_MERGE
     cudaEventDestroy(start);
